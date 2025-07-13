ce8bd3e82c3c6d7587676cfc17b9d11f9cca8811
josuejito <josuejito@gmail.com>
2025-07-13 03:04:56 -0600

    Update cerrar_sesion.php

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
-session_unset();
 session_destroy();
+
+// También puedes prevenir cache aquí
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 header("Location: index.php");
 exit();
 ?>
