27b07ddd29a45529805dc1b223d36626368034fa
josuejito <josuejito@gmail.com>
2025-07-12 19:52:29 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,38 @@
 <?php
-$usuario = $_POST['usuario'];
-$contrasena = $_POST['contraseña'];
+// Validar que se haya enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contraseña'];
 
-$serverName = "tcp:cafeteriahn.database.windows.net,1433";
-$connectionOptions = [
-    "Database" => "cafeteria",
-    "Uid" => "josuejorge@cafeteriahn",
-    "PWD" => "Barcelona25"
-];
+    $serverName = "tcp:cafeteriahn.database.windows.net,1433";
+    $connectionOptions = [
+        "Database" => "cafeteria",
+        "Uid" => "josuejorge@cafeteriahn",
+        "PWD" => "Barcelona25"
+    ];
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-if (!$conn) {
-    die(print_r(sqlsrv_errors(), true));
-}
+    if (!$conn) {
+        die(print_r(sqlsrv_errors(), true));
+    }
 
-$sql = "SELECT * FROM Usuarios WHERE usuario = ? AND contrasena = ?";
-$params = array($usuario, $contrasena);
-$stmt = sqlsrv_query($conn, $sql, $params);
+    $sql = "SELECT * FROM Usuarios WHERE usuario = ? AND contrasena = ?";
+    $params = array($usuario, $contrasena);
+    $stmt = sqlsrv_query($conn, $sql, $params);
 
-if ($stmt === false) {
-    die(print_r(sqlsrv_errors(), true));
-}
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
 
-if (sqlsrv_has_rows($stmt)) {
-    // Login exitoso, redirigir a YouTube
-    header("Location: rocio.html");
-    exit();
-} else {
-    echo "Usuario o contraseña incorrectos.";
+    if (sqlsrv_has_rows($stmt)) {
+        // Redirigir a página protegida
+        header("Location: rocio.html");
+        exit();
+    } else {
+        // Regresar al login con mensaje de error
+        header("Location: index.php?error=1");
+        exit();
+    }
 }
 ?>
